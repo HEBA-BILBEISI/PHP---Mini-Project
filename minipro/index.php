f31d2292650dc30a1bc5e0706d65f5c132d599b2
Heba Bilbeisi <71770290+HEBA-BILBEISI@users.noreply.github.com>
2021-02-18 21:59:30 +0200

    Add files via upload
    last update..
    jenan code

--- minipro/index.php
+++ minipro/index.php
@@ -1,120 +1,74 @@
-
-<!doctype html>
-<html>
-    <head>
-        <title>gallery PHP</title>
-        <link href='simplelightbox-master/dist/simple-lightbox.min.css' rel='stylesheet' type='text/css'>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        
-        <script type="text/javascript" src="simplelightbox-master/dist/simple-lightbox.jquery.min.js"></script>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-  
-        <link href='style.css' rel='stylesheet' type='text/css'>
-    </head>
-    <body>
- 
-
-    <div class=bg-secondary>
-<nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color: #cde2f1;">
-</ul>
-  <ul class="navigation">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>PHP</title>
+    <style>
+body{
+ background-color: #ffdcdc;
+ color:#455c60;
+}
+.prof{
+  background-color: cadetblue;
+  color:#e7f1f2;
+}
+</style>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light " style="background-color: #e3f2fd;">
+<!-- <a class="navbar-brand mr-5" href="index.php">Navbar Jenan </a> -->
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
     <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-      <li class="nav-item active">
-        <a class="nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="index.php">Academy Gallery</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="AttendanceReport.php">Attendance Report</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="Dashboard.php">Dashboard</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
+       <?php
+                function menuList($name,$link) {
+                echo  ' <li class="nav-item active mr-2 "><a class="nav-link text-info" href='.$link.'>'.$name.'<span class="sr-only">(current)</span></a> </li>';
+                }
+                $name = array("Home", "Attendance Report Page", "Dashboard page", "Academy Gallery");
+                $link=array("index.php","attendanceReportPage.php","dashboardPage.php","academyGallery.php");
+                array_map("menuList", $name,$link);
+            ?>
     </ul>
-
   </div>
 </nav>
-<center><h1>Academy Gallery Page</h1></center
 <div class="container">
+<h1 class="text-center p-5">Trainees Page</h1>
+<div class="container-fluid">
+  <div class="row">
+  
+<?php include 'array.php'; 
+for($i=0;$i<count($dataStudent);$i++){
+ $image=array($dataStudent[$i]['image']);
+ $name=array($dataStudent[$i]['name']);
+ $birthday=array($dataStudent[$i]['birthday']);
+ echo '<div class="col-6 col-md-4 col-lg-3 mb-4"><div class="card mx-auto text-center">';
+
+foreach($image  as $k  => $v){
+ echo '<a href="#url"><img class="card-img-top" src="'.$v.'" alt="Sample Title"></a>';
+}
+foreach($name  as $k  => $v){
+ echo '<div class="card-body">';
+ echo '<h5 class="card-title">'.$v.'</h5>';
+}  
+foreach($birthday as $k  => $v){
+ echo '<p class="card-text">'.$v.'</p>'; 
+ echo '<a href="traineePage.php?id='.$i.'" class="btn prof">Profile </a>';
+ echo '</div></div>';
+} 
+echo '</div>';
+}
+
+?>
+      </div>
+    </div>
+
 </div>
-</div>
-
-    
-        <div class='container'>
-            <div class="gallery">
-              
-            <?php 
-            // Image extensions
-            $image_extensions = array("png","jpg","jpeg","gif");
-
-            // Target directory
-            $dir = 'images/';
-            if (is_dir($dir)){
-                
-                if ($dh = opendir($dir)){
-                    $count = 1;
-
-                    // Read files
-                    while (($file = readdir($dh)) !== false){
-
-                        if($file != '' && $file != '.' && $file != '..'){
-                            
-                            // Thumbnail image path
-                            $thumbnail_path = "images/thumbnail/".$file;
-
-                            // Image path
-                            $image_path = "images/".$file;
-                            
-                            $thumbnail_ext = pathinfo($thumbnail_path, PATHINFO_EXTENSION);
-                            $image_ext = pathinfo($image_path, PATHINFO_EXTENSION);
-
-                            // Check its not folder and it is image file
-                            if(!is_dir($image_path) && 
-                                in_array($thumbnail_ext,$image_extensions) && 
-                                in_array($image_ext,$image_extensions)){
-                                ?>
-
-                                <!-- Image -->
-                                <a href="<?php echo $image_path; ?>">
-                                    <img src="<?php echo $thumbnail_path; ?>" alt="" title=""/>
-                                </a>
-                                <!-- --- -->
-                                <?php
-
-                                // Break
-                                if( $count%4 == 0){
-                                ?>
-                                    <div class="clear"></div>
-                                <?php    
-                                }
-                                $count++;
-                            }
-                        }
-                            
-                    }
-                    closedir($dh);
-                }
-            }
-            ?>
-            </div>
-        </div>
-
-
-        <!-- Script -->
-        <script type='text/javascript'>
-        $(document).ready(function(){
-
-            // Intialize gallery
-            var gallery = $('.gallery a').simpleLightbox();
-        });
-        </script>
-    </body>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
 </html>
